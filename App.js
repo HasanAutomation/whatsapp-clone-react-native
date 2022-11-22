73a9aff7fdb90cfa734a7f21f10f34a2cd1f802d
Hasan Ali <hasan@lacritz.ai>
2022-11-22 21:55:28 +0530

    Added splash screen and fonts

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appLoaded, setAppLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require('./assets/fonts//Roboto-Black.ttf'),
+          blackItalic: require('./assets/fonts/Roboto-BlackItalic.ttf'),
+          bold: require('./assets/fonts/Roboto-Bold.ttf'),
+          boldItalic: require('./assets/fonts/Roboto-BoldItalic.ttf'),
+          italic: require('./assets/fonts/Roboto-Italic.ttf'),
+          light: require('./assets/fonts/Roboto-Light.ttf'),
+          lightItalic: require('./assets/fonts/Roboto-LightItalic.ttf'),
+          medium: require('./assets/fonts/Roboto-Medium.ttf'),
+          mediumItalic: require('./assets/fonts/Roboto-MediumItalic.ttf'),
+          regular: require('./assets/fonts/Roboto-Regular.ttf'),
+          thin: require('./assets/fonts/Roboto-Thin.ttf'),
+          thinItalic: require('./assets/fonts/Roboto-ThinItalic.ttf'),
+        });
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setAppLoaded(true);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appLoaded) await SplashScreen.hideAsync();
+  }, [appLoaded]);
+
+  if (!appLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+      <SafeAreaView>
+        <Text style={styles.txt}>
+          Open up App.js to start working on your app! Hasan fuckkkkkkkk
+        </Text>
+        <Button title='Click Me' onPress={() => console.log('Fuck you')} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +57,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  txt: {
+    color: 'red',
+    fontFamily: 'black',
+    fontSize: 25,
   },
 });
